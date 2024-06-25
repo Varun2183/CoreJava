4980adee82aa08858ac54399bc40fa30c6267a4d
varun <varun@GROOT>
2024-06-25 18:53:18 +0530

    25-6-2024_1

---
+++ b96java/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+//Program to demonstrate encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum=10;//Data_variables
+	String name;
+	int age;
+	
+	void show()// Member Function
+	{
+		System.out.println("Serialnumber : "+serialNum+"\t Name : "+name+"\t Age : "+age);
+		
+	}
+	
+}
