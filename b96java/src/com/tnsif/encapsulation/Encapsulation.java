1d3ffec93551a7a64ae81d2cc7e5239aa9740375
varun <varun@GROOT>
2024-06-25 19:38:50 +0530

    25-6-24_2

--- b96java/src/com/tnsif/encapsulation/Encapsulation.java
+++ b96java/src/com/tnsif/encapsulation/Encapsulation.java
@@ -2,14 +2,38 @@
 package com.tnsif.encapsulation;
 
 public class Encapsulation {
-	int serialNum=10;//Data_variables
-	String name;
-	int age;
-	
+	//if no access specifier is mentioned then it ill be default
+	private int serialNum=10;//Data_variables
+	private String name;
+	private int age;
+	//using getter and setter 
 	void show()// Member Function
 	{
 		System.out.println("Serialnumber : "+serialNum+"\t Name : "+name+"\t Age : "+age);
 		
+	}
+	public int getSerialNum() {
+		return serialNum;
+	}
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	//to display string representation of the object
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
 	}
 	
 }
--- b96java/src/com/tnsif/encapsulation/EncapsulationDemo.java
+++ b96java/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -3,12 +3,18 @@ package com.tnsif.encapsulation;
 public class EncapsulationDemo {
 
 	public static void main(String[] args) {
-		//TODO Auto-generated
+		//TODO Auto-generated method stub
 		
 		Encapsulation ob= new Encapsulation();// object creation
-		ob.name="varun";//calling the variable
+	/*	ob.name="varun";//calling the variable
 		ob.age=33;
-		ob.serialNum=22;
+		ob.serialNum=22; */
+		
+		
+		ob.setAge(21);
+		ob.setName("arun");
+		ob.setSerialNum(2);
+		System.out.println(ob);
 		ob.show();
 	}
 }
