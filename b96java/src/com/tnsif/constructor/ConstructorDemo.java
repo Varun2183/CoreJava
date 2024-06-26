a156efd06dfa438e72a34b25c1cacfff7b4468ab
Varun2183 <varunyadav2183@gmail.com>
2024-06-26 19:55:41 +0530

    26-6-2024

---
+++ b96java/src/com/tnsif/constructor/ConstructorDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.constructor;
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+	
+		Customer ob=new Customer(2,"varun");
+		ob.setId(44);
+		ob.setName("arun");
+		System.out.println(ob);
+	}
+}
---
+++ b96java/src/com/tnsif/constructor/Customer.java
@@ -0,0 +1,42 @@
+package com.tnsif.constructor;
+
+public class Customer {
+
+
+	private int id;
+	private String name;
+	
+	//default constructor
+	public Customer() {
+		
+		super();
+		System.out.println("---Default Constuctor---");
+	}
+		
+	public Customer(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + "]";
+	}
+}
